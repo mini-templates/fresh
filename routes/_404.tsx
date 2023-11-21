a6a71f90f81422a40c57b48ca9f3685a9e1d4c15
interrrp <int@addictedto.coffee>
2023-11-21 17:01:07 +0800

    Initial commit

---
+++ routes/_404.tsx
@@ -0,0 +1,12 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Error404() {
+  return (
+    <>
+      <Head>
+        <title>404 - Page not found</title>
+      </Head>
+      <p>404 - Page not found</p>
+    </>
+  );
+}
